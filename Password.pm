e3bd455e643b1d60992a75f0cef32c9e8ae20f1c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-29 17:14:55 +0200

    Add SEE ALSO section to doc

--- Password.pm
+++ Password.pm
@@ -325,6 +325,16 @@ L<Tags::HTML::Page::End>,
 L<Tags::Output::Raw>,
 L<Unicode::UTF8>.
 
+=head1 SEE ALSO
+
+=over
+
+=item L<Plack::App::Login>
+
+Plack login application.
+
+=back
+
 =head1 REPOSITORY
 
 L<https://github.com/michal-josef-spacek/Plack-App-Login-Password>
